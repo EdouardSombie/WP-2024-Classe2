ee1cdbfcfbc5bd5aad722f749613af1a5ef872f7
Edouard <edouard@voltajazz.com>
2024-05-16 12:35:05 +0200

    intégration css :)

--- wordpress-2/ESGI-theme/index.php
+++ wordpress-2/ESGI-theme/index.php
@@ -1,35 +1,42 @@
 <?php get_header(); ?>
 
 <main id="site-content">
-    <?php
-    get_template_part('template-parts/identity-card');
-    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <?php
+                get_template_part('template-parts/identity-card');
+                ?>
 
-    <?php
-    if (!is_front_page()) {
-        // Si pas sur la page d'accueil : afficher les articles
+                <?php
+                if (!is_front_page()) {
+                    // Si pas sur la page d'accueil : afficher les articles
 
-        $args = [
-            'numberposts' => 6,
-            'orderby' => 'date',
-            'order' => 'DESC'
-        ];
-        $posts = get_posts($args);
+                    $args = [
+                        'numberposts' => 6,
+                        'orderby' => 'date',
+                        'order' => 'DESC'
+                    ];
+                    $posts = get_posts($args);
 
-        echo '<ul>';
-        foreach ($posts as $p) { ?>
+                    echo '<ul class="postsList">';
+                    foreach ($posts as $p) { ?>
 
-            <li>
-                <a href="<?= get_permalink($p) ?>">
-                    <span><?= $p->post_title ?></span>
-                    <time><?= wp_date('j F Y', strtotime($p->post_date))  ?></time>
-                </a>
-            </li>
+                        <li>
+                            <a href="<?= get_permalink($p) ?>">
+                                <span><?= $p->post_title ?></span>
+                                <time><?= wp_date('j F Y', strtotime($p->post_date))  ?></time>
+                            </a>
+                        </li>
 
-    <?php }
-        echo '</ul>';
-    }
-    ?>
+                <?php }
+                    echo '</ul>';
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
 
 </main>
 
